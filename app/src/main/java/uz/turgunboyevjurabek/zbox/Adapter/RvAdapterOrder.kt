2525fed7735986bf4afa8d64d584fd9f6c3d16cf
Asus <turgunboyevjurabek777@gmail.com>
2023-10-09 23:33:03 +0500

    4 tajriba

--- app/src/main/java/uz/turgunboyevjurabek/zbox/Adapter/RvAdapterOrder.kt
+++ app/src/main/java/uz/turgunboyevjurabek/zbox/Adapter/RvAdapterOrder.kt
@@ -1,18 +1,25 @@
 package uz.turgunboyevjurabek.zbox.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import retrofit2.Call
+import retrofit2.Response
 import uz.turgunboyevjurabek.zbox.databinding.ItemOrderRvBinding
 import uz.turgunboyevjurabek.zbox.madels.Order.Order_get
 import uz.turgunboyevjurabek.zbox.madels.Product.Product_Get_with_ID
+import uz.turgunboyevjurabek.zbox.network.ApiClinet
+import javax.security.auth.callback.Callback
 
-class RvAdapterOrder(val context: Context, val list2:ArrayList<Order_get>,val list: ArrayList<Product_Get_with_ID>  ):RecyclerView.Adapter<RvAdapterOrder.Vh>() {
+class RvAdapterOrder(val context: Context, val list2:ArrayList<Order_get>? ):RecyclerView.Adapter<RvAdapterOrder.Vh>() {
     inner class Vh(val itemOrderRvBinding: ItemOrderRvBinding):ViewHolder(itemOrderRvBinding.root){
+        fun onBind(order_get: Order_get,position: Int){
 
-        fun onBind(order_get: Order_get,position: Int,productGetWithId: Product_Get_with_ID){
+            itemOrderRvBinding.itemOrderName.text=order_get.sana
 
         }
 
@@ -22,10 +29,10 @@ class RvAdapterOrder(val context: Context, val list2:ArrayList<Order_get>,val li
         return Vh(ItemOrderRvBinding.inflate(LayoutInflater.from(parent.context),parent,false))
     }
 
-    override fun getItemCount(): Int =list2.size
+    override fun getItemCount(): Int =list2!!.size
 
     override fun onBindViewHolder(holder: Vh, position: Int) {
 
-        holder.onBind(list2[position],position,list[position])
+        holder.onBind(list2!![position],position)
     }
 }
